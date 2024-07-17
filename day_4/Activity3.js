1b2c7dc8f38b73fe147740859f9755412d8b7df0
Iqra khan . <ik22082002khan@gmail.com>
2024-07-17 14:59:18 +0530

    day_4

---
+++ day_4/Activity3.js
@@ -0,0 +1,22 @@
+//Activity 3 Do while loop
+
+//Task 5
+
+let num = 1;
+
+do {
+    console.log(num);
+    num++;
+} while (num <= 5)
+
+
+
+//Task 6 
+let num1 = 9;
+let i = 1;
+let fact = 0;
+do {
+    fact = fact + i;
+    i++;
+} while (i <= num1)
+console.log(fact);
---
+++ day_4/Activity4.js
@@ -0,0 +1,14 @@
+//Activity 4 nested loop 
+
+//Task 7 print pattern 
+
+let n= 5;
+
+for(let i = 0; i<n; i++){
+    for(let j = 0; j<=i; j++){
+        process.stdout.write(" * ");
+      
+    }
+    console.log();
+   
+}
