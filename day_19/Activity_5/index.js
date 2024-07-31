8a6e55e7de76800bb008b7575f2117f806e3ba62
Iqrakhan1 <ik22082002khan@gmail.com>
2024-08-01 00:11:37 +0530

    day_19 challange

--- day_19/Activity_5/index.js
+++ day_19/Activity_5/index.js
@@ -1,3 +1,12 @@
 // Activity 5: Practical Applications
 // Task 9: Write a regular expression to validate a simple password (must include at least one uppercase letter, one lowercase letter, one digit, and one special character). Log whether the password is valid.
+
+const regex = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[\W_]).{8,}$/;
+const password = "Passw0rd!";
+const isValidPassword = regex.test(password);
+console.log(isValidPassword); // true
 // Task 10: Write a regular expression to validate a URL. Log whether the URL is valid.
+let regexurl = /(https?|ftp):\/\/[^\s/$.?#].[^\s]*$/;
+const url = "https://www.example.com";
+const checkUrl = regexurl.test(url);
+console.log(checkUrl);
